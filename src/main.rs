7f79b4f5136bd774901f25141156f4d9e69a10eb
Michael Alls <allsjr@gmail.com>
2023-07-19 10:02:32 -0400

    another way of getting pokemon array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Debug, Deserialize)]
+struct Pokemon {
+    name: String,
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let res: serde_json::Value =
+        reqwest::blocking::get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+            .unwrap()
+            .json()
+            .unwrap();
+
+    let json = json!(res["results"]);
+
+    let pokemon: Vec<Pokemon> = serde_json::from_value(json).unwrap();
+
+    println!("Response: {:?}", pokemon);
 }
