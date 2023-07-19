c9335f538deea82b43db2543f7f96ff7947e98b4
Michael Alls <allsjr@gmail.com>
2023-07-19 17:16:10 -0400

    working code for caching data

--- src/main.rs
+++ src/main.rs
@@ -27,40 +27,33 @@ fn main() {
 
         let mut pokemon: Vec<Pokemon> = serde_json::from_value(json).unwrap();
 
-        pokemon
-            .iter_mut()
-            .enumerate()
-            .take(1)
-            .for_each(|(i, poke)| {
-                println!("getting sprite url for #{}-{}", i + 1, poke.name);
+        pokemon.iter_mut().enumerate().for_each(|(i, poke)| {
+            println!("getting sprite url for #{}-{}", i + 1, poke.name);
 
-                let sprite: serde_json::Value =
-                    reqwest::blocking::get(&poke.url).unwrap().json().unwrap();
+            let sprite: serde_json::Value =
+                reqwest::blocking::get(&poke.url).unwrap().json().unwrap();
 
-                let sprite_url = sprite["sprites"]["front_default"].as_str();
-                poke.sprite = Some(sprite_url.unwrap().to_string());
+            let sprite_url = sprite["sprites"]["front_default"].as_str();
+            poke.sprite = Some(sprite_url.unwrap().to_string());
 
-                // println!("{}", sprite_url.unwrap());
+            // println!("{}", sprite_url.unwrap());
 
-                let url = Url::parse(sprite_url.unwrap()).unwrap();
+            let url = Url::parse(sprite_url.unwrap()).unwrap();
 
-                let res = reqwest::blocking::get(url);
-                let bytes = res.unwrap().bytes().unwrap();
-                // println!("{:?}", bytes);
+            let res = reqwest::blocking::get(url);
+            let bytes = res.unwrap().bytes().unwrap();
+            // println!("{:?}", bytes);
 
-                println!("saving sprite for #{}-{}", i + 1, poke.name);
+            println!("saving sprite for #{}-{}", i + 1, poke.name);
 
-                let image_path = format!("images/image{}.png", i + 1);
-                let mut file = File::create(image_path).unwrap();
-                file.write_all(&bytes).unwrap();
-            });
+            let image_path = format!("images/image{}.png", i + 1);
+            let mut file = File::create(image_path).unwrap();
+            file.write_all(&bytes).unwrap();
+        });
 
-        // todo!("write data to json file");
-
+        // write data to json file
         let pokemon_string = serde_json::to_string(&pokemon).unwrap();
-
         let path = "pokemon.json";
-
         let mut output = File::create(path).unwrap();
         write!(output, "{pokemon_string}").unwrap();
     }
